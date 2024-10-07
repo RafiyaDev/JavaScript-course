e9555108c249aad045242f2165aa06480624bd0f
RafiyaDev <rafiya.webdev@gmail.com>
2024-10-07 10:51:06 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,19 @@
+const accountId = 20160051888
+let accountEmail = "rafiyahj7373@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+ //accountId = 2 // not allowed or cannot changed 
+
+
+ /*
+Prefer not to use var
+because of issue in block scope amd functional scope
+ */
+
+ accountEmail = "hc22hc@gmail.com"
+ accountPassword = 2932
+ accountCity = "New Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as version
+
+// alert(" 3 + 3") // We are using NodeJs not browser 
+
+console.log(3 + 3);
+
+console.log(3 + 
+    3); // code readablity should be high
+
+console.log("hello")
+
+let name = "rafiya"
+let age = 18
+let isLoggenIn = true
+
+
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof ""); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
