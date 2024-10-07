fa2f84a8cf364149b8b2b46640bdce5a7811c0ce
RafiyaDev <rafiya.webdev@gmail.com>
2024-10-07 11:34:39 +0000

    Convertion of datatypes

---
+++ 01-basics/03-convertionoperation.js
@@ -0,0 +1,31 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+// "false" => 0
+
+
+let isLoggedIn = 1
+
+//let BooleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(BooleanisLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "rafiya" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
