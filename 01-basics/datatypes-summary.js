0412a4762b1bcd4466d02c75ad671ce0f2c3ba64
RafiyaDev <rafiya.webdev@gmail.com>
2024-10-10 13:04:55 +0000

    datatypes-summary

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types : String, Number, BigInt, Boolean, Object, Symbol, Null.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTem = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 67326833222132321333n
+
+
+
+// Reference type(Non primitive):
+
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "rafiya",
+    age: 60,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof id);
+console.log(typeof anotherId);
+
+
