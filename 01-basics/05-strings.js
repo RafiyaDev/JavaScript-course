3fdbf19e4cb880dbc22618e2add032222a17d52e
RafiyaDev <rafiya.webdev@gmail.com>
2024-10-14 16:14:28 +0000

    changes

--- 01-basics/05-strings.js
+++ 01-basics/05-strings.js
@@ -5,6 +5,15 @@ const age =  50
 
 console.log(`hello my name is ${name} and my Age is ${age}`);
 
+//const nameOne = "RAFIYAKHAN"
+//const ageOne = 60
+//const id = 2017827868
+//const email = "rafiyak@gmail.com"
+
+//console.log(`hello my name is ${nameOne} and my age is ${ageOne} and my id is ${id} and my email is ${email}`)
+
+
+
 const gameName = new String("RAFIYAKHAN")
 
 //console.log(gameName);
@@ -14,12 +23,18 @@ console.log(gameName.length);
 //console.log(gameName.toUpperCase);
 console.log(gameName.charAt(2));
 console.log((gameName.indexOf('F')));
+console.log(gameName.slice(-8,  8))
+console.log(gameName.substring(0, 4))
+
+///----------------------------------------///
 
 const newString = gameName.substring(0, 4);
 console.log(newString);
 
 const anotherString = gameName.slice(-8, 8)
 console.log(anotherString);
+
+
 
 
 
