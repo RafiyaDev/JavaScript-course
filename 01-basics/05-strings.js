fe739ebb1371cc5174bebcf95c5ca55bab24d486
RafiyaDev <rafiya.webdev@gmail.com>
2024-10-14 13:41:30 +0000

    changes

---
+++ 01-basics/05-strings.js
@@ -0,0 +1,27 @@
+const name = "rafiya"
+const age =  50
+
+//console.log(name + age + "hello");
+
+console.log(`hello my name is ${name} and my Age is ${age}`);
+
+const gameName = new String("RAFIYAKHAN")
+
+//console.log(gameName);
+//console.log(gameName.__proto__);
+
+console.log(gameName.length);
+//console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log((gameName.indexOf('F')));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 8)
+console.log(anotherString);
+
+
+
+
+
--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -2,20 +2,20 @@
 
 // 7 types : String, Number, BigInt, Boolean, Object, Symbol, Null.
 
-const score = 100
-const scoreValue = 100.3
+//const score = 100
+//const scoreValue = 100.3
 
-const isLoggedIn = false
+//const isLoggedIn = false
 const outsideTem = null
-let userEmail;
+//let userEmail;
 
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+//const id = Symbol('123')
+//const anotherId = Symbol('123')
 
 //console.log(id === anotherId);
 
-const bigNumber = 67326833222132321333n
+//const bigNumber = 67326833222132321333n
 
 
 
@@ -23,17 +23,49 @@ const bigNumber = 67326833222132321333n
 
 // Arrays, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "rafiya",
-    age: 60,
+//const heros = ["shaktiman", "naagraj", "doga"];
+//let myObj = {
+  //  name: "rafiya",
+    //age: 60,
+//}
+
+//const myFunction = function(){
+  //  console.log("Hello world");
+//}
+
+//console.log(typeof id);
+//console.log(typeof anotherId);
+
+
+/// ***********************************************************************************************///
+
+// typeof memory:
+
+// 1) Stack (Primitive) , 2) Heap (Non-Primitive)
+
+
+let myYoutubename = "rafkhan89329"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "userone873.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+let userTwo = userOne
 
-console.log(typeof id);
-console.log(typeof anotherId);
+userTwo.email = "rafiyakhan@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
 
 
