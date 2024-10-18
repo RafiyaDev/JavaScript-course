73dc141cb3a4ef48def87df548dc9b120b953469
RafiyaDev <rafiya.webdev@gmail.com>
2024-10-18 08:46:56 +0000

    changes

--- 01-basics/stringobjects.js
+++ 01-basics/stringobjects.js
@@ -50,10 +50,28 @@ console.log(indexof.indexOf("o"));
 let match = "hello, hello, hello, hello, hello"
 let result = match.match(/ll/g);
 console.log(result);
-
 let match2 = "The rain in Spain stays mainly in the plain."
 let result2 = match2.match(/ain/g);
 console.log(result2);
+
+
+let replace = "HELLO WORLD HERE I'M"
+let changes = replace.replace("WORLD", "CODERS")
+console.log(changes);
+let replace2 = "HELLLLLO CODERS"
+console.log(replace2.replace("HELLLLLO", "HELLO"));
+
+
+let search = "The rain in SPAIN stays mainly in the plain"
+console.log(search.search("rain"));
+let search2 = "HELLO"
+let changes2 = search2.search("LL")
+console.log(changes2);
+
+
+let slice = "hello world"
+console.log(slice.slice(0, 8));
+
 
 
 
